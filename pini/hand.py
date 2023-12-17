db6c4f4bfdf9b8e24b12b7bbb283c8ab24997f19
Pini Koplovitch <pini.koplovitch@gmail.com>
2023-12-17 22:58:50 +0200

    refactor the Hand class

--- pini/hand.py
+++ pini/hand.py
@@ -1,28 +1,54 @@
+from collections import Counter
+
 from die import Die
 
 
 class Hand:
-    def __init__(self, n_dice=5, n_sides=6):
-        self.n_dice = n_dice
-        self.n_sides = n_sides
-        self.hand = [Die(self.n_sides) for _ in range(self.n_dice)]
+    def __init__(self, dice: int = 5, sides: int = 6):
+        """_summary_
 
-    def roll(self, dice):
-        if not all(1 <= num <= self.n_dice for num in dice):
-            raise IndexError("Dice must be between 1 and " + str(self.n_dice))
-        for die in dice:
-            self.hand[die - 1] = Die(self.n_sides)
+        Args:
+            dice (int, optional): The number of dice in the hand. Defaults to 5.
+            sides (int, optional): the Number of sides each die has. Defaults to 6.
+        """
+        self.dice = dice
+        self.sides = sides
+        self.hand = [Die(self.sides) for _ in range(self.dice)]
 
-    def get_hand(self):
+    def roll(self, dice: list[int]) -> None:
+        """rolls the specified dice
+
+        Args:
+            dice (list[int]): dice to roll
+
+        Raises:
+            IndexError: if a die that is not in the hand
+        """
+        if not all(1 <= num <= self.dice for num in dice):
+            raise IndexError("Dice must be between 1 and " + str(self.dice))
+        for i in dice:
+            self.hand[i - 1].roll()
+
+    def get_hand(self) -> list[int]:
+        """Returns the hand as a list of integers"""
         return [die.get_face() for die in self.hand]
 
     def set_hand(self, values):
-        self.hand = [Die(self.n_sides, val) for val in values]
+        """Sets the hand to the specified values"""
+        self.hand = [Die(self.sides, val) for val in values]
 
-    def count_occurrences(self, face):
-        return self.get_hand().count(face)
+    @property
+    def counter(self) -> Counter:
+        """Returns a Counter of the values in the hand"""
+        return Counter(self.get_hand())
 
-    def sum_hand(self):
+    def count_occurrences(self, face) -> int:
+        """Returns the number of occurrences of the specified face in the hand"""
+        return self.counter[face]
+
+    @property
+    def sum(self) -> int:
+        """Returns the sum of the values in the hand"""
         return sum(self.get_hand())
 
     def __str__(self) -> str:
