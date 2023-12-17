2dde1a00bbf5bb010100f2101d57bc99d584249c
Pini Koplovitch <pini.koplovitch@gmail.com>
2023-12-17 09:11:32 +0200

    remove roll method, instead of rolling creating a new die

--- pini/die.py
+++ pini/die.py
@@ -3,21 +3,18 @@ from typing import Optional
 
 
 class Die:
-    def __init__(self, face: Optional[int] = None, sides=6):
+    def __init__(self, sides=6, face: Optional[int] = None):
         self.sides = sides
         if face is not None:
-            self.__face = face
+            self.set_face(face)
         else:
-            self.roll()
+            self.set_face(random.randint(1, self.sides))
 
     def set_face(self, value: int):
         self.__face = value
 
     def get_face(self):
         return self.__face
-
-    def roll(self):
-        self.set_face(random.randint(1, self.sides))
 
     def __str__(self):
         return str(self.__face)
