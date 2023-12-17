ddbcd0aaeae0a3bd9274fa0d6853cfcff29b77a3
Pini Koplovitch <pini.koplovitch@gmail.com>
2023-12-17 22:29:51 +0200

    refactor Die class

--- pini/die.py
+++ pini/die.py
@@ -5,15 +5,20 @@ from typing import Optional
 class Die:
     def __init__(self, sides=6, face: Optional[int] = None):
         self.sides = sides
-        if face is not None:
-            self.set_face(face)
-        else:
-            self.set_face(random.randint(1, self.sides))
+        self.roll() if face is None else self.set_face(face)
 
-    def set_face(self, value: int):
+    def roll(self) -> None:
+        """Rolls the Die"""
+        self.__face = random.randint(1, self.sides)
+
+    def set_face(self, value: int) -> None:
+        """Sets the face of the Die"""
+        if value not in range(1, self.sides + 1):
+            raise ValueError(f"Face value must be between 1 and {self.sides}")
         self.__face = value
 
-    def get_face(self):
+    def get_face(self) -> int:
+        """Returns the face of the Die"""
         return self.__face
 
     def __str__(self):
