5ed0d5eb85b6c7eb19fd0d5057f4186d982d8c9e
Pini Koplovitch <pini.koplovitch@gmail.com>
2023-12-20 12:26:58 +0200

    refactor rules

---
+++ pini/rules/base.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from hand import Hand
+
+
+@dataclass
+class Rule:
+    rule_name: str
+
+    def name(self):
+        return self.rule_name
+
+    def points(self, hand: Hand) -> int:
+        ...
+
+    def to_score(self, hand: Optional[Hand] = None) -> bool:
+        ...
+
+    def score(self, hand: Hand) -> int:
+        return self.points(hand) if self.to_score(hand) else 0
+
+
+@dataclass
+class SameValueRule(Rule):
+    value: int
+
+    def points(self, hand: Hand) -> int:
+        return hand.count_occurrences(self.value) * self.value
+
+    def to_score(self, hand) -> bool:
+        return True
+
+
+@dataclass
+class NOfAKind(Rule):
+    value: int
+
+    def points(self, hand: Hand) -> int:
+        return hand.sum
+
+    def to_score(self, hand: Hand) -> bool:
+        return max(hand.counter.values()) >= self.value
+
+
+@dataclass
+class FixPointsRule(Rule):
+    _points: int
+
+    def points(self, hand: Hand) -> int:
+        return self._points
+
+
+@dataclass
+class Straight(FixPointsRule):
+    size: int
+
+    def to_score(self, hand: Hand) -> bool:
+        dice = set(hand.get_hand())
+        return (len(dice) == self.size) and (max(dice) - min(dice) == (self.size - 1))
---
+++ pini/rules/yahtzee.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass
+
+from hand import Hand
+from rules.base import FixPointsRule, NOfAKind, Rule, SameValueRule, Straight
+
+
+@dataclass
+class Aces(SameValueRule):
+    rule_name: str = "Aces"
+    value: int = 1
+
+
+@dataclass
+class Twos(SameValueRule):
+    rule_name: str = "Twos"
+    value: int = 2
+
+
+@dataclass
+class Threes(SameValueRule):
+    rule_name: str = "Threes"
+    value: int = 3
+
+
+@dataclass
+class Fours(SameValueRule):
+    rule_name: str = "Fours"
+    value: int = 4
+
+
+@dataclass
+class Fives(SameValueRule):
+    rule_name: str = "Fives"
+    value: int = 5
+
+
+@dataclass
+class Sixes(SameValueRule):
+    rule_name: str = "Sixes"
+    value: int = 6
+
+
+@dataclass
+class ThreeOfAKind(NOfAKind):
+    rule_name: str = "Three of a kind"
+    value: int = 3
+
+
+@dataclass
+class FourOfAKind(NOfAKind):
+    rule_name: str = "Four of a kind"
+    value: int = 4
+
+
+@dataclass
+class FullHouse(FixPointsRule):
+    rule_name: str = "Full house"
+    _points: int = 25
+
+    def to_score(self, hand: Hand) -> bool:
+        return set([2, 3]) == set(hand.counter.values())
+
+
+@dataclass
+class SmallStraight(Straight):
+    rule_name: str = "Small straight"
+    size: int = 4
+    _points: int = 30
+
+
+@dataclass
+class LargeStraight(Straight):
+    rule_name: str = "Large straight"
+    size: int = 5
+    _points: int = 40
+
+
+@dataclass
+class Yahtzee(FixPointsRule):
+    rule_name: str = "Yahtzee"
+    _points: int = 50
+
+    def to_score(self, hand: Hand) -> bool:
+        return len(set(hand.get_hand())) == 1
+
+
+@dataclass
+class FibonYahtzee(FixPointsRule):
+    rule_name: str = "FibonYahtzee"
+    _points: int = 100
+
+    def to_score(self, hand: Hand) -> bool:
+        return sorted(hand.get_hand()) == [1, 1, 2, 3, 5]
+
+
+@dataclass
+class Chance(Rule):
+    rule_name: str = "Chance"
+
+    def points(self, hand: Hand) -> int:
+        return hand.sum
+
+    def to_score(self, hand: Hand) -> bool:
+        return True
