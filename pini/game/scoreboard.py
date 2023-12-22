99fccce11044f098a6d4997f57a1534041c14c3a
Pini Koplovitch <pini.koplovitch@gmail.com>
2023-12-22 15:53:20 +0200

    adding type hints

--- pini/game/scoreboard.py
+++ pini/game/scoreboard.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Set
 
 from game import Hand
 from game.rules import Rule
@@ -7,31 +7,35 @@ from game.rules import Rule
 
 @dataclass
 class GameRule:
+    """Class to store Rule information"""
+
     rule: Rule
     points: Optional[int] = None
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """print the name of the rule"""
         return self.rule.name
 
 
 class ScoreBoard:
     def __init__(self, rules):
-        self.rules = self.register_rules(rules)
-        self.rules_used = set()
+        self.rules: Dict[int, GameRule] = self.__register_rules(rules)
+        self.rules_used: Set[int] = set()
 
     @property
-    def rules_count(self):
+    def rules_count(self) -> int:
+        """Count total number of rules in the scoreboard"""
         return len(self.rules)
 
     @classmethod
-    def register_rules(cls, rules_list: List[Rule]) -> Dict:
+    def __register_rules(cls, rules_list: List[Rule]) -> Dict[int, GameRule]:
         return {i: GameRule(rule=rule) for i, rule in enumerate(rules_list, start=1)}
 
-    def get_rule(self, row: int):
+    def get_rule(self, row: int) -> Rule:
         return self.rules[row].rule
 
-    def assign_points(self, row: int, hand: Hand):
+    def assign_points(self, row: int, hand: Hand) -> int:
         if self.rules[row].points is not None:
             raise Exception("ScoreBoard already saved!")
         points = self.rules[row].rule.assign_points(hand)
@@ -39,12 +43,12 @@ class ScoreBoard:
         return points
 
     @property
-    def total_points(self):
+    def total_points(self) -> int:
         return sum(
             rule.points for rule in self.rules.values() if rule.points is not None
         )
 
-    def view_points(self, hand: Optional[Hand] = None):
+    def view_points(self, hand: Optional[Hand] = None) -> str:
         strs = []
         padding = max(len(rule.name) for rule in self.rules.values())
         for idx, rule in self.rules.items():
